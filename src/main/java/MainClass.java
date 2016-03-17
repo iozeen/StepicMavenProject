062cdb32e904e020fb3d244470eaafbc414aa324
iozeen <iozintv@gmail.com>
2016-03-17 17:02:54 +0200

    init

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Illya on 17.03.2016.
+ */
+public class MainClass {
+    public static void main(String[] args){
+        System.out.println("file to be added to repo...");
+    }
+}
---
+++ src/test/java/TestClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Illya on 17.03.2016.
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("test class to be added to repo...");
+    }
+}
