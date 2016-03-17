d8bb6930e1ba9aed46d13bcf7b522d82d7658e5d
iozeen <iozintv@gmail.com>
2016-03-17 17:11:01 +0200

    another commit

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -4,5 +4,7 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("test class to be added to repo...");
+        System.out.println("test class to be added to repo...");
+        System.out.println("test class to be added to repo...");
     }
 }
